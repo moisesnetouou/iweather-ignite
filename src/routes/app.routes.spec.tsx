3e2d8bde804c37073a50907f24eb7a2242491380
Moisés <moisesnetored@gmail.com>
2023-10-26 17:18:49 -0400

    test: finishing route testing

--- src/routes/app.routes.spec.tsx
+++ src/routes/app.routes.spec.tsx
@@ -1,10 +1,35 @@
-import { render } from "@testing-library/react-native"
+import { act, render, screen, waitFor } from "@__tests__/utils/customRender"
 import { Routes } from "."
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { api } from "@services/api"
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
 
 describe("Routes", () => {
-  it("should be render search screen when not city selected", ()=> {
-    const {debug} = render(<Routes />)
+  it("should be render search screen when not city selected", async ()=> {
+    render(<Routes />)
 
-    debug()
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+
+    expect(title).toBeTruthy()
+  })
+
+  it("should be render dashboard screen when has city selected", async ()=> {
+    jest.spyOn(api, "get").mockResolvedValue({
+      data: mockWeatherAPIResponse
+    })
+
+    const city = {
+      id: "1",
+      name: "São Paulo",
+      latitude: 123,
+      longitude: 456
+    }
+    
+    await saveStorageCity(city)
+    
+    await act(() => waitFor(() => render(<Routes />)))
+
+    const title = screen.getByText(city.name)
+    expect(title).toBeTruthy()
   })
 })
