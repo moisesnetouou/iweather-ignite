ecc8041152764fcc57fce43736e3378da07331e0
Moisés Neto <moisesnetored@gmail.com>
2023-10-26 22:41:01 -0400

    test: search screen

---
+++ src/screens/Search/Search.spec.tsx
@@ -0,0 +1,18 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityApiResponse"
+import { fireEvent, render, screen, waitFor } from "@__tests__/utils/customRender"
+import {Search} from "@screens/Search"
+import { api } from "@services/api"
+
+describe("Screen: Search", ()=> {
+  it("should be show city option",async ()=> {
+    jest.spyOn(api, "get").mockResolvedValue({data: mockCityAPIResponse})
+    const {debug} = render(<Search />)
+
+    const searchInput = screen.getByTestId("search-input")
+    fireEvent.changeText(searchInput, "São Paulo")
+
+    const option = await waitFor(() => screen.findByText(/são paulo/i))
+    debug()
+    expect(option).toBeTruthy()
+  })
+})
